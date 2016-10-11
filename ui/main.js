058f4bcb384ac87c3f5bfe6a290ac22d3f165bc4
girishsuri <girishsuri27@gmail.com>
2016-10-11 19:23:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,7 @@ element.innerHTML = 'New value';
 
 //Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
